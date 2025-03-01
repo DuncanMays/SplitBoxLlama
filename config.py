6cd365c66b2f62512eab00455c7d7da5b6cc05ca
Duncan <duncanmays@outlook.com>
2025-02-28 21:07:51 -0500

    factored down worker and removed duplicate code

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+from sys import argv as args
+
+def get_arg(default_arg, arg_tag):
+    if arg_tag in args:
+        index = args.index(arg_tag)
+        return args[index + 1]
+
+    else:
+        return default_arg
+
 # Update the MASTER_CONFIG with batch_size and context_window parameters
 MASTER_CONFIG = {
     'batch_size': 32,        # Number of batches to be processed at each random split
@@ -5,6 +15,6 @@ MASTER_CONFIG = {
     'd_model': 128,          # dimension of linear layers
     'epochs': 5000//2,          # Number of training epochs
     'log_interval': 500,      # Log information every 10 batches during training
-    'n_layers': 4,
+    'n_layers': int(get_arg(4, '-n')),
     'n_heads': 8,
 }
