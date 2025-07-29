226922972938f75f4495469df736ef8189607931
Duncan <duncanmays@outlook.com>
2025-07-28 20:57:05 -0400

    removed events.py, factored out the plotting code to plot_pipeline.py

--- pipeline_parallel.py
+++ pipeline_parallel.py
@@ -1,13 +1,47 @@
 import asyncio
-import random
-import time
 
-from matplotlib import pyplot as plt
+class EventFlow():
 
-from events import EventFlow, print_msg
+	def __init__(self):
+		self.event_dict = {}
+		self.actions = []
+		self.start_event = asyncio.Event()
 
-task_timings = []
-global_start = time.time()
+	async def set_action_helper(self, triggers, callbacks, events):
+
+		if (len(triggers) == 0):
+			triggers = [self.start_event]
+
+		T = [t.wait() for t in triggers]
+		await asyncio.gather(*T)
+
+		await asyncio.gather(*callbacks)
+
+		for e in events:
+			e.set()
+
+	def get_event(self, event_name):
+		e = None
+
+		if event_name in self.event_dict:
+			e = self.event_dict[event_name]
+
+		else:
+			e = asyncio.Event()
+			self.event_dict[event_name] = e
+
+		return e
+
+	def set_action(self, triggers, callbacks, events):
+
+		triggers = [self.get_event(t) for t in triggers]
+		events = [self.get_event(e) for e in events]
+
+		self.actions.append(self.set_action_helper(triggers, callbacks, events))
+
+	async def start(self):
+		self.start_event.set()
+		await asyncio.gather(*self.actions)
 
 def parse_task_str(task_str):
 	d = task_str[0]
@@ -51,47 +85,6 @@ def get_prior_task(task_str, num_workers, num_stages):
 
 	return f'{prior_d}{w}s{prior_s}'
 
-async def fake_task(task_str, t):
-
-	start = time.time() - global_start
-
-	print(f'starting {task_str} for {t} seconds')
-	await asyncio.sleep(t)
-	print(f'{task_str} ended')
-
-	end = time.time() - global_start
-
-	data_point = {"task_str": task_str, "start": start, "end": end}
-	task_timings.append(data_point)
-
-def plot_timings():
-	print("plotting!")
-
-	f_x = []
-	f_tops = []
-	f_bottoms = []
-	b_x = []
-	b_tops = []
-	b_bottoms = []
-
-	for dp in task_timings:
-		d, w, s = parse_task_str(dp["task_str"])
-
-		if (d=="f"):
-			f_x.append(w)
-			f_tops.append(dp["end"] - dp["start"])
-			f_bottoms.append(dp["start"])
-
-		if (d=="b"):
-			b_x.append(w)
-			b_tops.append(dp["end"] - dp["start"])
-			b_bottoms.append(dp["start"])
-
-	plt.bar(f_x, f_tops, bottom=f_bottoms, edgecolor="black", color="blue")
-	plt.bar(b_x, b_tops, bottom=b_bottoms, edgecolor="black", color="red")
-
-	plt.show()
-
 def get_pipeline_parallel_flow(num_workers, get_pipeline_stages, batch):
 	
 	pipeline_prefixes = []
@@ -134,28 +127,3 @@ def get_pipeline_parallel_flow(num_workers, get_pipeline_stages, batch):
 			flow.set_action(triggers, callbacks, events)
 
 	return flow
-
-
-async def main():
-
-	num_workers = 3
-
-	# delta = lambda : random.random()/50+0.1
-	delta = lambda : 0.1
-	def get_pipeline_stages(j):
-		pipeline_stages = []
-		for i in range(num_workers): pipeline_stages.append(fake_task(task_str=f'f{i+1}s{j+1}', t=0.5*delta()))
-		for i in range(num_workers): pipeline_stages.append(fake_task(task_str=f'b{num_workers-i}s{j+1}', t=1*delta()))
-
-		return pipeline_stages
-
-	batch = [1,2,3,4,5]
-
-	flow = get_pipeline_parallel_flow(num_workers, get_pipeline_stages, batch)
-
-	await flow.start()
-
-	plot_timings()
-
-if (__name__ == '__main__'):
-	asyncio.run(main())
---
+++ plot_pipeline.py
@@ -0,0 +1,76 @@
+import asyncio
+import random
+import time
+
+from matplotlib import pyplot as plt
+
+from pipeline_parallel import get_pipeline_parallel_flow, parse_task_str
+
+task_timings = []
+global_start = time.time()
+
+async def fake_task(task_str, t):
+
+	start = time.time() - global_start
+
+	print(f'starting {task_str} for {t} seconds')
+	await asyncio.sleep(t)
+	print(f'{task_str} ended')
+
+	end = time.time() - global_start
+
+	data_point = {"task_str": task_str, "start": start, "end": end}
+	task_timings.append(data_point)
+
+def plot_timings():
+	print("plotting!")
+
+	f_x = []
+	f_tops = []
+	f_bottoms = []
+	b_x = []
+	b_tops = []
+	b_bottoms = []
+
+	for dp in task_timings:
+		d, w, s = parse_task_str(dp["task_str"])
+
+		if (d=="f"):
+			f_x.append(w)
+			f_tops.append(dp["end"] - dp["start"])
+			f_bottoms.append(dp["start"])
+
+		if (d=="b"):
+			b_x.append(w)
+			b_tops.append(dp["end"] - dp["start"])
+			b_bottoms.append(dp["start"])
+
+	plt.bar(f_x, f_tops, bottom=f_bottoms, edgecolor="black", color="blue")
+	plt.bar(b_x, b_tops, bottom=b_bottoms, edgecolor="black", color="red")
+
+	plt.show()
+
+async def main():
+
+	num_workers = 3
+	num_stages = 15
+
+	delta = lambda : random.random()/50+0.1
+	# delta = lambda : 0.1
+	def get_pipeline_stages(j):
+		pipeline_stages = []
+		for i in range(num_workers): pipeline_stages.append(fake_task(task_str=f'f{i+1}s{j+1}', t=0.5*delta()))
+		for i in range(num_workers): pipeline_stages.append(fake_task(task_str=f'b{num_workers-i}s{j+1}', t=1*delta()))
+
+		return pipeline_stages
+
+	batch = list(i+1 for i in range(num_stages))
+
+	flow = get_pipeline_parallel_flow(num_workers, get_pipeline_stages, batch)
+
+	await flow.start()
+
+	plot_timings()
+
+if (__name__ == '__main__'):
+	asyncio.run(main())
