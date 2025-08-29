2daeb7f16b2bd200c0a86e7c958904c75d5b0fef
Duncan <duncanmays@outlook.com>
2025-08-29 15:18:44 -0400

    added OR tools solver for allocation

--- allocation.py
+++ allocation.py
@@ -3,6 +3,8 @@ import torch
 import uuid
 import time
 
+from ortools.linear_solver import pywraplp
+
 from pipeline_parallel import get_pipeline_parallel_flow, parse_task_str
 from plot_pipeline import metrics_wrapper, plot_timings
 from benchmark import benchmark, MockWorker
@@ -33,6 +35,48 @@ def round_with_sum_constraint(floats, target_sum=None):
 
     return floored
 
+def delay(b, c, d):
+    if b == 0:
+        return b*c
+    else:
+        return b*c + d
+
+def allocate(benchmark_scores, num_blocks):
+
+    num_scores = len(benchmark_scores)
+    solver = pywraplp.Solver.CreateSolver('GLOP')  # 'GLOP' is for linear programming
+
+    # variables
+    block_allocations = []
+    for i in range(num_scores):
+        block_allocations.append(solver.NumVar(0, solver.infinity(), f'b{i}'))
+
+    max_delay = solver.NumVar(0, solver.infinity(), f'max_delay')
+
+    # constraints
+    solver.Add(sum(block_allocations) == num_blocks)
+    
+    for i in range(num_scores):
+        b = block_allocations[i]
+        c = benchmark_scores[i][0]
+        d = benchmark_scores[i][1]
+
+        solver.Add(delay(b, c, d) <= max_delay)
+
+    # objective function
+    solver.Minimize(max_delay)
+
+    # Solve the problem
+    status = solver.Solve()
+
+    # Output results
+    if status == pywraplp.Solver.OPTIMAL:
+        print(f'Objective value = {solver.Objective().Value()}')
+    else:
+        print('The problem does not have an optimal solution.')
+
+    return [b.solution_value() for b in block_allocations]
+
 async def main():
 
     num_workers = 3
@@ -44,21 +88,25 @@ async def main():
     stubs = [MockWorker(forward_rate=c, backward_rate=1.2*c, download_rate=d) for c, d in zip(worker_compute_rates, worker_download_rates)]
 
     # benchmark workers
+    print('benchmarking!')
     benchmark_promises = [benchmark(stub, torch.randn([32, 28, 28]), 20) for stub in stubs]
     benchmark_scores = await asyncio.gather(*benchmark_promises)
 
     C = [score[0] for score in benchmark_scores]
     D = [score[1] for score in benchmark_scores]
+    print("benchmark scores: ", C, D)
 
     # each worker is allocated a number of blocks so that the expected delays of all workers are the same
-    expected_delays = [total_blocks*c + d for c, d in zip(C, D)]
-    t = [round(1/t) for t in expected_delays]
+    relative_delays = [total_blocks*c + d for c, d in zip(C, D)]
+    print("relative delays: ", relative_delays)
 
-    print(expected_delays)
-    print(t)
-    allocations = round_with_sum_constraint(t, target_sum=total_blocks)
-    print(allocations)
-    for i in range(num_workers): stubs[i].set_blocks(allocations[i])
+    allocations = allocate(benchmark_scores, total_blocks)
+    print('allocations:', allocations)
+    allocations = round_with_sum_constraint(allocations, target_sum=total_blocks)
+    print('rounded allocations:', allocations)
+
+    expected_delays = [delay(b, c, d) for b, c, d in zip(allocations, C, D)]
+    print("expected delays: ", expected_delays)
 
     exit()
 
