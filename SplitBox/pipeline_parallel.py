9e1fc412b7c8520eb8dc9eddd885901b8fc1c915
duncan <duncanmays@outlook.com>
2026-03-11 18:42:54 -0400

    EventFlow removes old events from _fired

--- SplitBox/pipeline_parallel.py
+++ SplitBox/pipeline_parallel.py
@@ -9,6 +9,7 @@ class EventFlow:
 		self._pending = []    # (triggers, callbacks, events) queued before start()
 		self._started = False
 		self._tasks = set()
+		self._trigger_refs = {}  # trigger -> count of actions not yet scheduled that reference it
 
 	def _add_to_tasks(self, coro):
 		task = asyncio.create_task(coro)
@@ -25,15 +26,23 @@ class EventFlow:
 				self._emitter.emit(name)
 
 		def check_and_launch():
+
 			if fired_so_far >= set(triggers):
 				self._add_to_tasks(run_callbacks())
+
+				for trigger in triggers:
+					self._trigger_refs[trigger] -= 1
+					
+					if self._trigger_refs[trigger] <= 0:
+						del self._trigger_refs[trigger]
+						self._fired.discard(trigger)
 
 		def make_listener(trigger):
 
 			def listener():
 				fired_so_far.add(trigger)
 				check_and_launch()
-				
+
 			return listener
 
 		if not triggers:
@@ -44,13 +53,15 @@ class EventFlow:
 
 			if trigger in self._fired:
 				fired_so_far.add(trigger)
-				
+
 			else:
 				self._emitter.on(trigger, make_listener(trigger))
 
 		check_and_launch()
 
 	def set_action(self, triggers, callbacks, events):
+		for trigger in triggers:
+			self._trigger_refs[trigger] = self._trigger_refs.get(trigger, 0) + 1
 
 		if self._started:
 			self._schedule_action(triggers, callbacks, events)
